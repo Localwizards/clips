345c4c8e20aa3909ac86d378d230a4267d2ce81a
Localwizards <christiancampbell2002@gmail.com>
2025-11-01 10:23:22 -0700

    auto_update clips on 25-11-01

--- auto_push.sh
+++ auto_push.sh
@@ -3,5 +3,17 @@
 ls -1t ./DEADLOCK/*.mp4 > videos.txt
 mv videos.txt ./DEADLOCK/videos.txt
 
+ls -1t ./MH4U/*.mp4 > videos.txt
+mv videos.txt ./MH4U/videos.txt
+
+
+set -e
+git add .
+today=$(date +%y-%m-%d)
+
+git commit -m "auto_update clips on $today"
+git push -u origin main
+
+echo "success"
 
 
